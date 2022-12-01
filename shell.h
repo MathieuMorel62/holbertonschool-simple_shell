567d1754212e2afa5a2075d54c5e8545f146575f
MathieuMorel62 <mormth@aol.com>
2022-12-01 01:48:44 -0800

    adding the shell file with  the different library

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef __SHELL__
+#define __SHELL__
+
+
+/* ========= LIBRARY ========= */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+/* ======== PROTOTYPES ======== */
+
+extern char **environ;
+
+
+#endif
