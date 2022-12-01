8770215ba666b6fa4c9eaca4ba59b83dbfb399b1
kerrodia <5658@holbertonstudents.com>
2022-12-01 06:32:24 -0800

    example shell.c and shell.h

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef __SHELL__
-#define __SHELL__
+#ifndef SHELL_H
+#define SHELL_H
 
 
 /* ========= LIBRARY ========= */
@@ -9,11 +9,35 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include <stddef.h>
+#include <signal.h>
 
 
-/* ======== PROTOTYPES ======== */
+/* ======== ENVIRONMENT ======== */
 
 extern char **environ;
 
+/* STRUCTS */
 
-#endif
+/**
+ * struct builtins - struct with builtin commands
+ * @argv: arg passed as command
+ * @func: correlations function
+ * description: this is to handle the builtins
+ */
+typedef struct builtins
+{
+	char *argv;
+	int (*func)();
+} builtins;
+
+char **tokenize(char *userinput);
+int shell_loop(int argc, char **argv);
+int _putchar(char c);
+
+
+#endif /* SHELL_h */
