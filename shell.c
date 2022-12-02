b37ff068b140bac852bd876e54fff760636f039c
kerrodia <5658@holbertonstudents.com>
2022-12-02 02:03:14 -0800

    tokenize

--- shell.c
+++ shell.c
@@ -1,67 +1,10 @@
-#include "shell.h"
-
-/**
- * main - main function for our simple shell
- * description: main
- * @argc: argc
- * @argv: argv
- * Return: 0
- */
-int main(int argc, char **argv)
-{
-	shell_loop(argc, argv);
-	return (0);
-}
-
-/**
- * shell_loop - shell loop to constantly run the shell
- * description: until the exit command is entered
- * @argc: argc
- * @argv: argv
- * Return: 0
- */
-int shell_loop(int argc, char **argv)
-{
-	int userinput;
-	char *buffer = NULL;
-	size_t bufsize;
-	char *prompt = "$ ";
-	char **path_tokens = NULL;
-	char *executable = NULL;
-
-	(void) argc;
-
-	buffer = NULL;
-	while (1)
-	{
-		/* buffer = malloc(sizeof(char) * bufsize);*/
-		   /* if (buffer == NULL) */
-		   /* perror("Malloc failure\n"); */
-		if (isatty(STDIN_FILENO))
-		{
-			write(STDOUT_FILENO, prompt, stringlength(prompt));
-		}
-		userinput = getline(&buffer, &bufsize, stdin);
-		if (userinput == -1)
-			break;
-		argv = tokenize(buffer);
-		if (argv[0] == NULL)
-			continue;
-		if (function_finder(argv, buffer) == 1)
-		{
-			free(argv);
-	free(buffer);
-		}
-return (0);
-}
-
 /**
  * tokenize - tokenize user input
  * description: to breakdown command
- * @userinput: userinput
+ * @line: line
  * Return: 0
  */
-char **tokenize(char *userinput)
+char **tokenize(char *line)
 {
 	int token_inc = 0;
 	char *tokenize = NULL;
@@ -69,10 +12,10 @@ char **tokenize(char *userinput)
 	int tokencount = 0;
 	int i;
 
-	strtok(userinput, "\n");
-	for (i = 0; userinput[i] != '\0'; i++)
+	strtok(line, "\n");
+	for (i = 0; line[i] != '\0'; i++)
 	{
-		if (userinput[i] == ' ')
+		if (line[i] == ' ')
 		{
 			tokencount++;
 		}
