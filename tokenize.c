007ce5f6b63d44a1dc60819c9d73162476f0f380
CaroChoch <5692@holbertonstudents.com>
2022-12-02 03:40:06 -0800

    rename file

---
+++ tokenize.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * tokenize - tokenize user input
+ * description: to breakdown command
+ * @line: line
+ * Return: 0
+ */
+char **tokenize(char *line)
+{
+	int token_inc = 0;
+	char *tokenize = NULL;
+	char **argv = NULL;
+	int tokencount = 0;
+	int i;
+
+	strtok(line, "\n");
+	for (i = 0; line[i] != '\0'; i++)
+	{
+		if (line[i] == ' ')
+		{
+			tokencount++;
+		}
+	}
+	argv = malloc(8 * (tokencount + 2));
+	if (argv != NULL)
+	{
+		token_inc = 0;
+		tokenize = strtok(userinput, " ");
+		while (token_inc < (tokencount + 1))
+		{
+			argv[token_inc] = tokenize;
+			tokenize = strtok(NULL, " ");
+			token_inc++;
+		}
+		argv[token_inc] = NULL;
+	}
+
+	/*free(userinput);*/
+	return (argv);
+}
