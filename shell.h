0faf8c70e1c968ce9660813f19f5c02e53b41680
CaroChoch <5692@holbertonstudents.com>
2022-12-02 03:37:55 -0800

    refresh prototypes

--- shell.h
+++ shell.h
@@ -17,30 +17,12 @@
 #include <stddef.h>
 #include <signal.h>
 
-/* ======== ENVIRONMENT ======== */
+/* ======== PROTOTYPES ======== */
 
 extern char **environ;
-
 int _putchar(char c);
 int exec(char **args);
 char *f_read(void);
-
-/* STRUCTS */
-
-/**
- * struct builtins - struct with builtin commands
- * @argv: arg passed as command
- * @func: correlations function
- * description: this is to handle the builtins
- */
-typedef struct builtins
-{
-	char *argv;
-	int (*func)();
-} builtins;
-
 char **tokenize(char *userinput);
-int shell_loop(int argc, char **argv);
-int _putchar(char c);
 
-#endif /* SHELL_h */
+#endif /* SHELL_H */
