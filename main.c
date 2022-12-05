d8b11a416633b58d9b4da896f17bf205261d9dda
CaroChoch <5692@holbertonstudents.com>
2022-12-05 04:33:28 -0800

    add signal and environment

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * main - start the shell
+ * main - starts the shell
  *
  * Return: 0
  */
@@ -9,43 +9,41 @@
 int main(void)
 {
 	int status = 1, index = 0, j = 0;
-	char *line;
-	char **args = NULL;
+	char *line, **args = NULL;
 
+/* signal : librairy - SIGINT is the interrupt signal (ctrl+C) */
+	signal(SIGINT, _signal);
 	while (status != 0)
 	{
-		status = isatty(0);
-
+/*isatty is a function that returns 1 if file descriptor refers to a terminal*/
+		status = isatty(STDIN_FILENO);
 		if (status == 1)
-		{
-			write(1, "#cisfun& ", 9);
-		}
+			write(STDOUT_FILENO, "#cisfun$ ", 10); /* prompt is "#Cisfun$" */
 		line = f_read();
-
-		while (line[j] != '\0')
+		for (j = 0; line[j] != '\0'; j++)
 		{
 			if (line[index] == ' ')
-			{
 				index++;
-			}
-			j++;
 		}
 		if (line[index] == '\0')
 		{
 			free(line);
 			continue;
 		}
-		args = tokenize(line);
-
-		if (args == NULL)
+		if (_strcmp(line, "env") == 0)
+		{
+			print_env();
+			free(line);
+			continue;
+		}
+			args = tokenize(line);
+			if (args == NULL)
 		{
 			free(line);
 			continue;
 		}
-		if (line[0] != '\n' || line[1] != '\0')
-		{
+			if (line[0] != '\n' || line[1] != '\0')
 			status = exec(args);
-		}
 		free(args);
 		free(line);
 	}
