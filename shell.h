0c83fe5313241af3ff6576bbe83c90411b810612
CaroChoch <5692@holbertonstudents.com>
2022-12-05 04:33:57 -0800

    add librairies and prototypes

--- shell.h
+++ shell.h
@@ -1,37 +1,31 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef __SHELL__
+#define __SHELL__
 
 
-/* ============ LIBRARY ============= */
+/* ========= LIBRARY ========= */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <signal.h>
+#include <sys/stat.h>
 #include <sys/types.h>
-#include <stdarg.h>
-#include <string.h>
 #include <errno.h>
-#include <stddef.h>
 #include <signal.h>
 
-
-/* ======= AUXILIAR FUNCTIONS ======== */
-
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
-int _strncmp(char *s1, char *s2, size_t len);
-
-/* =========== PROTOTYPES =========== */
+/* ======== PROTOTYPES ======== */
 
 extern char **environ;
+
 int _putchar(char c);
 int exec(char **args);
 char *f_read(void);
 char **tokenize(char *line);
+char *get_env(char *command);
+char **split_env(char *path);
+void print_env(void);
+char *check_path(char *command);
+void _signal(int sig);
 
 #endif
