cbfc0d97ee0632e3e8f0a841447c2054729de90f
MathieuMorel62 <mormth@aol.com>
2022-12-08 08:09:23 -0800

    fix bug

--- error.c
+++ error.c
@@ -7,19 +7,19 @@
 * @cmd: command
 */
 
-void _perror(char *name, char *cmd)
+void _perror(char *name, char *command)
 {
 	char *msg = NULL;
 	int length = 0;
 
-	length = sizeof(char) * (_strlen(name) + _strlen(cmd) + 20);
+	length = sizeof(char) * (_strlen(name) + _strlen(command) + 20);
 	msg = malloc(length);
 	if (!msg)
 		exit(0);
 	msg[0] = '\0';
 	_strcat(msg, name);
 	_strcat(msg, ": ");
-	_strcat(msg, cmd);
+	_strcat(msg, command);
 	_strcat(msg, " not found\n");
 	write(2, msg, length - 1);
 	free(msg);
